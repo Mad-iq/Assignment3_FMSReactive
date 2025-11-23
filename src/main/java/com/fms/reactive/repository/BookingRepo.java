d891b8a17bfe5d9c8877461d1756f8980ff927eb
Mad-iq <debashritamandal852@gmail.com>
2025-11-24 00:48:52 +0530

    added cutom methods to the repository layers

--- src/main/java/com/fms/reactive/repository/BookingRepo.java
+++ src/main/java/com/fms/reactive/repository/BookingRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.fms.reactive.model.Booking;
 
+import reactor.core.publisher.Flux;
+
 public interface BookingRepo extends ReactiveMongoRepository<Booking, String> {
 
+	Flux<Booking> findByEmail(String email);
 }
--- src/main/java/com/fms/reactive/repository/FlightRepo.java
+++ src/main/java/com/fms/reactive/repository/FlightRepo.java
@@ -1,9 +1,19 @@
 package com.fms.reactive.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.fms.reactive.model.Flight;
 
+import reactor.core.publisher.Flux;
+
 public interface FlightRepo extends ReactiveMongoRepository<Flight, String> {
 
+	 Flux<Flight> findBySourceAndDestinationAndStartTimeBetween(
+	            String source,
+	            String destination,
+	            LocalDateTime start,
+	            LocalDateTime end
+	    );
 }
